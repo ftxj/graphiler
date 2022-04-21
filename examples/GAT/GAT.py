010e141092ba9a37e61a9e1e35a129ccda9c9f0d
ftxj <932141413@qq.com>
2022-04-21 15:43:25 +0800

    only split

--- examples/GAT/GAT.py
+++ examples/GAT/GAT.py
@@ -37,7 +37,7 @@ def reduce_func(nodes: NodeBatchDummy):
     return {'h': h}
 
 
-mpdfg = mpdfg_builder(message_func, reduce_func, -1)
+mpdfg = mpdfg_builder(message_func, reduce_func, opt_level=-1)
 #mpdfg_compile = mpdfg_builder(message_func, reduce_func, opt_level=0)
 #mpdfg_plus_reorder = mpdfg_builder(message_func, reduce_func, opt_level=1)
 
