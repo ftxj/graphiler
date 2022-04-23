8752cff26317bdb207468e5490d8b772e07cdfe3
ftxj <932141413@qq.com>
2022-04-23 15:34:25 +0800

    test

--- examples/GCN/GCN_PyG.py
+++ examples/GCN/GCN_PyG.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 
+from graphiler.utils import setup
+
 class GCN_PyG(nn.Module):
     def __init__(self,
                  in_dim,
@@ -19,3 +21,18 @@ class GCN_PyG(nn.Module):
         h = F.relu(h)
         h = self.layer2(h, adj)
         return h
+
+device = setup()
+
+feat_dim = 500
+hidden_dim = 32
+out_dim = 12
+
+net_dgl = GCN_PyG(in_dim=feat_dim, hidden_dim=hidden_dim, out_dim=out_dim).to(device)
+
+script_model = torch.jit.script(net_dgl)
+
+print("DGL Graph")
+print(script_model.graph)
+print("DGL Inlined Graph")
+print(script_model.inlined_graph)
