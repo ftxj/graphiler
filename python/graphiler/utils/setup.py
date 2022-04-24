8cc95ed6f3946a6b317e63b5beadf758ccb7112e
ftxj <932141413@qq.com>
2022-04-24 14:59:11 +0800

    test

--- python/graphiler/utils/setup.py
+++ python/graphiler/utils/setup.py
@@ -43,6 +43,7 @@ def load_data(name, feat_dim=DEFAULT_DIM, prepare=True, to_homo=True):
         g = dataset[0]
         print(g)
         print(type(g))
+        exit()
     elif name == "debug":
         g = dgl.graph(([0, 0, 0, 1, 1, 2], [0, 1, 2, 1, 2, 2]))
     elif name == 'aifb':
