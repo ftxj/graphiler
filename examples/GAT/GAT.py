2b9b1777c2dd3cfff1cc351f53636b4f3ef07f8b
ftxj <932141413@qq.com>
2022-04-25 19:35:53 +0800

    test

--- examples/GAT/GAT.py
+++ examples/GAT/GAT.py
@@ -33,8 +33,7 @@ def message_func(edges: EdgeBatchDummy, fc_weight, attn_weight):
 
 
 def reduce_func(nodes: NodeBatchDummy):
-    print(type(nodes))
-    print(len(nodes))
+    print(nodes))
     alpha = torch.softmax(nodes.mailbox['e'], dim=1)
     h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
     return {'h': h}
