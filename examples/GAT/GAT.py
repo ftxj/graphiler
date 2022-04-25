b59c4fa8d55c919fa38e0ae82a1cd1e259ba518f
ftxj <932141413@qq.com>
2022-04-25 19:40:45 +0800

    test

--- examples/GAT/GAT.py
+++ examples/GAT/GAT.py
@@ -24,7 +24,8 @@ BREAK_FLAG = 2
 # therefore, we pass extra parameters as a workaround for class member
 # e.g., self.fc_weight, compare with GATLayer.message_func for the difference
 def message_func(edges: EdgeBatchDummy, fc_weight, attn_weight):
-    print(edges.src)
+    x = edges.src['h']
+    print(x)
     z_s = torch.mm(edges.src['h'], fc_weight)
     z_d = torch.mm(edges.dst['h'], fc_weight)
     z2 = torch.cat([z_s, z_d], dim=1)
