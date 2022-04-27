0039fe8eb5aef4ee492c2389b078935cc38328ab
ftxj <932141413@qq.com>
2022-04-27 11:10:33 +0800

    test

--- gatconv.py
+++ gatconv.py
@@ -30,6 +30,10 @@ if isinstance(feat, tuple):
     h_dst = feat_drop(feat[1])
 else:
     h_src = h_dst = feat_drop(feat)
+    print(h_src.type)
+    
+    print(h_dst.type)
+    
     feat_src = feat_dst = fc(h_src).view(-1, num_heads, out_feats)
     if graph.is_block:
         feat_dst = feat_src[:graph.number_of_dst_nodes()]
